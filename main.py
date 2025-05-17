ce8e36053a6e8d91ba90e32376a6ef0204346256
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-05-17 17:42:33 +0500

    fitness website

--- main.py
+++ main.py
@@ -5,5 +5,29 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/templates/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/templates/services.html")
+def services():
+    return render_template('services.html')
+
+@app.route("/templates/contact.html")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/templates/gallery.html")
+def gallery():
+    return render_template('gallery.html')
+
+@app.route("/templates/login.html")
+def login():
+    return render_template('login.html')
+
+@app.route("/templates/register.html")
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
