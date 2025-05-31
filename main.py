f8290484bb3149e1cb3ca73e04f8ae1e1d8eb9f0
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-06-01 01:54:54 +0500

    Fitness Website

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/templates/about.html")
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route("/templates/services.html")
+@app.route('/services')
 def services():
     return render_template('services.html')
 
-@app.route("/templates/contact.html")
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route("/templates/gallery.html")
+@app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
-@app.route("/templates/login.html")
+@app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route("/templates/register.html")
+@app.route('/register')
 def register():
     return render_template('register.html')
 
-@app.route("/templates/form.html")
+@app.route('/form')
 def form():
     return render_template('form.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
